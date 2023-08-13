64f65c72a478338e17a2bb6de9c7f4c3aa4636ff
Rub1_N <ananymousssssss123@gmail.com>
2023-08-13 17:46:45 +0200

    FlashLight is now an item and can be picked up

--- Assets/Scenes/Player/Flashlight/FlashlightSwitch.cs
+++ Assets/Scenes/Player/Flashlight/FlashlightSwitch.cs
@@ -9,25 +9,54 @@ public class FlashlightSwitch : MonoBehaviour
     public Light2D flashLight;
     public Light2D flashLightBackLight;
     public AudioClip flashLightOnSound;
-    
+
 
     private void Awake()
     {
         flashLight.enabled = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool ContainsFlashlight()
     {
-        if(Input.GetKeyDown(KeyCode.F))
+        if (Inventory.instance.ContainsItemOfName("Flashlight"))
+            return true;
+        return false;
+    }
+
+    public void RemoveFlashlight()
+    {
+        isOn = false;
+        flashLight.enabled = false;
+        flashLightBackLight.enabled = false;
+    }
+
+    public void EnableFlashlight()
+    {
+        if (ContainsFlashlight())
         {
             isOn = !isOn;
             flashLight.enabled = isOn;
             flashLightBackLight.enabled = isOn;
-            if(isOn)
+
+            if (isOn)
             {
                 SoundInstance.InstantiateOnTransform(flashLightOnSound, transform, 0.3f, true);
             }
+        }
+        else
+        {
+            isOn = false;
+            flashLight.enabled = false;
+            flashLightBackLight.enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            EnableFlashlight();
         }
     }
 }
--- Assets/Scenes/Player/Scripts/Item/Inventory.cs
+++ Assets/Scenes/Player/Scripts/Item/Inventory.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Inventory : MonoBehaviour
 {
@@ -8,16 +9,18 @@ public class Inventory : MonoBehaviour
 
     public static Inventory instance;
 
-    
-    private void Awake() {
-        if(instance != null)
+
+    private void Awake()
+    {
+        if (instance != null)
         {
             Debug.LogWarning("More than one instance of inventory found!");
-            return; 
+            return;
         }
         instance = this;
     }
     #endregion
+    public UnityEvent onItemChangedTrigger;
     public delegate void OnItemChanged();
     public OnItemChanged onItemChangedCallback;
     public List<Item> items = new List<Item>();
@@ -25,22 +28,36 @@ public class Inventory : MonoBehaviour
 
     public bool Add(Item item)
     {
-        if(items.Count >= space)
+        if (items.Count >= space)
         {
             Debug.Log("Not enough room to add the item!");
             return false;
         }
 
         items.Add(item);
-        if(onItemChangedCallback != null) onItemChangedCallback.Invoke();
-        
-        
+        if (onItemChangedCallback != null) onItemChangedCallback.Invoke();
+        if (onItemChangedTrigger != null) onItemChangedTrigger.Invoke();
+
+
         return true;
     }
 
     public void Remove(Item item)
     {
         items.Remove(item);
-        if(onItemChangedCallback != null) onItemChangedCallback.Invoke();
+        if (onItemChangedCallback != null) onItemChangedCallback.Invoke();
+        if (onItemChangedTrigger != null) onItemChangedTrigger.Invoke();
+    }
+
+    public bool ContainsItemOfName(string name)
+    {
+        for (int i = 0; i < items.Count; i++)
+        {
+            if (items[i].name == name)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
