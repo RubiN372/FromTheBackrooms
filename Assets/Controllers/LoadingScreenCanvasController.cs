6071309b9d9bd8ad1d41331d99494597c16fde24
Rub1_N <ananymousssssss123@gmail.com>
2024-06-25 16:04:56 +0200

    asasas

--- Assets/Controllers/LoadingScreenCanvasController.cs
+++ Assets/Controllers/LoadingScreenCanvasController.cs
@@ -5,6 +5,23 @@ using UnityEngine.UI;
 
 public class LoadingScreenCanvasController : MonoBehaviour
 {
+    #region Singleton
+    public static LoadingScreenCanvasController Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            parentObject.SetActive(false);
+        }
+    }
+    #endregion
     [SerializeField] Image image;
     [SerializeField] Image loadingCircle;
     [SerializeField] GameObject parentObject;
@@ -12,37 +29,33 @@ public class LoadingScreenCanvasController : MonoBehaviour
     bool lerping = false;
 
 
-    void Awake()
-    {
-        parentObject.SetActive(false);
-    }
-
-    void Start()
-    {
-        StartCoroutine(FadeTransition(4, true));
-    }
-
-    IEnumerator FadeTransition(float fadeTime, bool fadeIn)
+    public IEnumerator FadeTransition(float fadeTime, bool fadeIn)
     {
         lerping = true;
         float elapsedTime = 0;
+        yield return new WaitForSeconds(1f);
 
         if (fadeIn)
         {
+            Debug.Log("da");
+            parentObject.SetActive(true);
             while (elapsedTime < fadeTime)
             {
                 image.color = new Color(0, 0, 0, Mathf.Lerp(0, 1, elapsedTime));
                 elapsedTime += Time.deltaTime;
                 yield return null;
             }
+
         }
         else
         {
-
+            while (elapsedTime < fadeTime)
+            {
+                image.color = new Color(0, 0, 0, Mathf.Lerp(1, 0, elapsedTime));
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+            parentObject.SetActive(false);
         }
-
-
-
     }
-
 }
--- Assets/Scenes/Main Menu/Scripts/UI_Manager.cs
+++ Assets/Scenes/Main Menu/Scripts/UI_Manager.cs
@@ -5,6 +5,7 @@ public class UI_Manager : MonoBehaviour
     [SerializeField] GameObject loadingCircle;
     public void PlayButton()
     {
+        // StartCoroutine(LoadingScreenCanvasController.Instance.FadeTransition(1f, true));
         loadingCircle.SetActive(true);
         Loader.LoadAsync(Loader.Scene.PlayerScene, true);
         Loader.LoadAsync(Loader.Scene.Level_0, false);
